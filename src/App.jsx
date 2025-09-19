c1a6b8e3b79c33df2bd91913035d57defa287298
Pratyush <letscode269@gmail.com>
2025-09-20 01:06:14 +0530

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Main from './components/main/main'
+import Main from './components/Main/Main'
 import About from './components/About/About'
-import Services from './components/Services/services'
+import Services from './components/Services/Services'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
